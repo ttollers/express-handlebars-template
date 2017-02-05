32b030199bf057693636162ee214b0e10194e70c
ttollers <tom.tollinton@trinitymirror.com>
2017-02-05 14:15:25 +0000

    Remove unecessary stuff

--- server.js
+++ server.js
@@ -4,22 +4,12 @@ const express = require("express");
 var exphbs  = require('express-handlebars');
 
 let app = express();
-app.use(express.static('dist'));
+app.use(express.static('public'));
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-let endTime = '2017-02-04T19:30:00+0000';
-
-app.get("/update-end/:year/:month/:day/:hour/:min", (req, res) => {
-  const { year, month, day, hour, min} = req.params;
-  endTime = `${year}-${month}-${day}T${hour}:${min}:00+0000`;
-  res.send(`End time succesfully changed to ${endTime}`);
-});
-
 app.get('/', function (req, res) {
-  res.render("index", {
-    endTime: endTime
-  })
+  res.render("index")
 });
 
 app.listen(process.env.PORT || 8080);
